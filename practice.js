9318a8a8e49a7c042c7b8d4564c0335790679049
Anqush Jamdagani <njamdagani@gmail.com>
2017-09-18 17:43:45 +0530

    filter inventor born in 1500's

--- practice.js
+++ practice.js
@@ -5,9 +5,22 @@ var inventors = [
 	{first: 'Marie', last: 'Curie', year: '1867', passed: '1934'},
 	{first: 'Johannes', last: 'Kepler', year: '1571', passed: '1630'},
 	{first: 'Nicolaus', last: 'Copernicus', year: '1473', passed: '1543'},
-	{first: 'Max', last: 'Planck', year: '1858', passed: '1947'},
+	{first: 'Max', last: 'Planck', year: '1858', passed: '1947'}
 ];
 
 var people = [ 'Jamdagani, Naveen', 'Jamdagani, Ankush', 'Beck, Glenn', 'Becker, Call',
-'John, Adams', 'Benn, Tony', 'Wayne, Bruce'
+'John, Adams', 'Benn, Tony', 'Wayne, Bruce', 'Benjamin, Walter', 'Bellow, Soul',
+'Beneson, Peter', 'Bent, Siellas', 'Blake, William', 'Blair, Tony', 'Modi, Narendra',
+'Shah, Amit', 'Irani, Smriti', 'Thakrey, Bal', 'Kalaam, Abdul'
 ];
+
+//Array.prototype.filter()
+//1.Filter the list of inventors for those who were born in 1500's
+var fifteen = inventors.filter(function(inventor) {
+	if(inventor.year >=1500 && inventor.year < 1600) {
+		return true;//keep it
+	}
+});
+
+console.log(fifteen);
+console.table(fifteen);
